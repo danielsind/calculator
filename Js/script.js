189507a24078950c67c7152cf722b5a57a62c3bc
danielsind <danielsindet@gmail.com>
2019-11-28 10:27:31 +0300

    create a new js,html,css files

---
+++ Js/script.js
@@ -0,0 +1,6 @@
+var add = function (numb1,numb2){
+    return numb1 + numb2;
+    };
+    var numb1 =  parseInt(prompt  ("Enter Number"));
+    var numb2 = parseInt(prompt ("Enter another number"));
+    alert (add(numb1,numb2));
