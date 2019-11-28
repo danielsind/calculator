fa92119a6d4c5e8f0275879aec6ecf005b9c2753
danielsind <danielsindet@gmail.com>
2019-11-28 12:39:00 +0300

    delete readme

--- Js/script.js
+++ Js/script.js
@@ -3,4 +3,5 @@ var add = function (numb1,numb2){
     };
     var numb1 =  parseInt(prompt  ("Enter Number"));
     var numb2 = parseInt(prompt ("Enter another number"));
-    alert (add(numb1,numb2));
+    var result = add(numb1,numb2);
+    alert (result);
